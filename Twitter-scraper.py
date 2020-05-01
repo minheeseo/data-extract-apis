11ad2577deb29a5835c04747e49eeab422e2ace5
Min Hee Seo <minheeseo@Min-Hees-Mac-Pro.local>
2020-05-01 18:18:13 -0500

    feat: add a list of twitter handles

--- Twitter-scraper.py
+++ Twitter-scraper.py
@@ -2,7 +2,8 @@ import tweepy
 import csv
 import sqlite3
 
-# twitter error message
+
+# twitter error message (doc: https://readthedocs.org/projects/tweepy/downloads/pdf/latest/)
 def limit_handled(cursor):
     while True:
         try:
@@ -16,10 +17,12 @@ consumer_key=""
 consumer_secret=""
 access_token=""
 access_token_secret=""
+# authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
+# if you have list of twitter handles as a separate file, read that file
 """ name = []
 with open('politician.txt', mode='r') as csv_file:
 	csv_reader = csv.DictReader(csv_file)
@@ -29,3 +32,7 @@ with open('politician.txt', mode='r') as csv_file:
 			line_count +=1
 		name.append(row["name"])
 		line_count+=1 """
+
+# if not, create a list of twitterhandles.
+name = '@WhiteHouse'
+
